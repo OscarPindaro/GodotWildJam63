6b3ecb3220fa6b8d69f9706bd869d35c6b93fa0f
Oscar Pindaro <oscar.pindaro@gmail.com>
2023-11-11 18:34:40 +0100

    crocchetta now follows the mouse

--- scenes/minigames/Crocchette/CrocchetaBag.gd
+++ scenes/minigames/Crocchette/CrocchetaBag.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 var mouse_inside : bool = false
+var mouse_active: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,9 +10,22 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		# print("Mouse Click/Unclick at: ", event.position)
+		if event.pressed and mouse_inside:
+			mouse_active = true
+		else:
+			mouse_active = false
+			# else if not event.pressed and mouse_inside:
+			# 	mouse_active = 
+			# print(event)
+	
 
+func _process(delta):
+	if mouse_active:
+		position = get_viewport().get_mouse_position()
 
 func _on_mouse_entered():
 	mouse_inside = true
