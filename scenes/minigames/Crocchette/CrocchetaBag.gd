6b4cae1ec6d2f629a50913db15121b65c13f29e0
Oscar Pindaro <oscar.pindaro@gmail.com>
2023-11-11 23:42:43 +0100

    crocchetta scene working correctly

--- scenes/minigames/Crocchette/CrocchetaBag.gd
+++ scenes/minigames/Crocchette/CrocchetaBag.gd
@@ -3,6 +3,16 @@ extends Area2D
 var mouse_inside : bool = false
 var mouse_active: bool = false
 
+@export var crocchetta_scene: PackedScene
+@export var force_intensity :int = 30
+@export var min_delta_position : int = 5
+
+var rng = RandomNumberGenerator.new()
+
+var old_position: Vector2 = Vector2.ZERO
+
+var can_shoot : bool = true
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,14 +28,30 @@ func _input(event):
 			mouse_active = true
 		else:
 			mouse_active = false
-			# else if not event.pressed and mouse_inside:
-			# 	mouse_active = 
-			# print(event)
 	
 
 func _process(delta):
 	if mouse_active:
 		position = get_viewport().get_mouse_position()
+		var direction = (position - old_position).normalized()
+		
+		if can_shoot and (position - old_position).length() > min_delta_position:
+			can_shoot = false
+			$CrocTimer.start()
+			var n_crocs = rng.randi_range(2,4)
+			for i in range(n_crocs):
+				var croc: RigidBody2D = crocchetta_scene.instantiate()
+				add_child(croc)
+				var random_noise_x = 0
+				var random_noise_y = 0
+				croc.global_position = $SpawnPoint.global_position + Vector2(rng.randfn(10, 30),rng.randfn(10, 30))
+				var modified_direction = direction.rotated(rng.randf_range(-PI/6, PI/6))
+				# croc.add_constant_force(direction*force_intensity, croc.global_position)
+				croc.apply_central_impulse(modified_direction*force_intensity)
+				croc.apply_torque_impulse(rng.randf_range(-100,100))
+	old_position = position
+
+	
 
 func _on_mouse_entered():
 	mouse_inside = true
@@ -33,3 +59,7 @@ func _on_mouse_entered():
 
 func _on_mouse_exited():
 	mouse_inside = false
+
+
+func _on_croc_timer_timeout():
+	can_shoot = true
