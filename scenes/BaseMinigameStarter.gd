13dc9cc0bf8c4e564b613181de10c8b5fb010c5f
Oscar Pindaro <oscar.pindaro@gmail.com>
2023-11-12 13:37:06 +0100

    added samueles staff

--- scenes/BaseMinigameStarter.gd
+++ scenes/BaseMinigameStarter.gd
@@ -1,6 +1,9 @@
 extends BaseObject
 
 signal _start_minigame(minigame) 
+
+@export var minigamePath :Resource
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,9 +15,11 @@ func _process(delta):
 
 
 func _player_interact(player):
-	var minigame = $minigame
-	print(minigame)
-	_start_minigame.emit(minigame)
+	#var minigame = $minigame
+	#print(minigame)
+	#_start_minigame.emit(minigame)
+	var minigamePath = self.minigamePath #.resource_path
+	_start_minigame.emit(minigamePath)	
 	pass
 
 
